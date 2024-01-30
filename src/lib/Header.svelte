efc09f5ebeec8a5e7f3311ab84bd9c4cac78df90
nuliyev <jokerprodigy@gmail.com>
2024-01-30 10:26:20 +0500

    Fetch

---
+++ src/lib/Header.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+type PlanetItem = {
+	id: string;
+	name: string;
+};
+
+type PlanetsResponse = {
+	count: number;
+	data: PlanetItem[];
+};
+    export let data: PlanetsResponse;
+</script>
+
+<header>
+    <nav>
+        <menu>
+
+            {#each data.data as planet (planet.id)}
+            <li>
+                <a href={`/${planet.id}`}>{planet.name}</a>
+            </li>
+            {/each}
+
+        </menu>
+    </nav>
+</header>
